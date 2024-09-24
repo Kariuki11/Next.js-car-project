844293ebb29db8c6213d1b38b6bbbad6d4d03f1c
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-25 01:42:37 +0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="overflow">
+    <main className="overflow-hidden">
+      <Hero />
 
     </main>
   )
