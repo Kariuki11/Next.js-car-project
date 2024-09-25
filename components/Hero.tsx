23a06b6a5244125a41471681ffd8bae5f8f3b031
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-25 16:32:31 +0300

    new changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,6 +23,11 @@ const Hero = () => {
                 containerStyles="bg-primary-blue text-white rounded-full mt-10"
                 handleClick={handleScroll}
             />
+            <div className="hero__image-container">
+                <div className="hero__image">
+                    <Image src="/hero.png" alt="hero" fill className="object-contain" />
+                </div>
+            </div>
         </div>
     </div>
   )
