275adf89c7838f6b7987c90d0b09a0f530fd2b78
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-25 16:49:11 +0300

    new changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,30 +7,32 @@ const Hero = () => {
     const handleScroll = () => {
 
     }
+    
 
-  return (
-    <div className="hero">
-        <div className="flex-1 pt-36 padding-x">
-            <h1 className="hero__title"> Find, Book or Rent a Car Quickly and Easily!!
-            </h1>
+//   return (
+//     <div className="hero">
+//         <div className="flex-1 pt-36 padding-x">
+//             <h1 className="hero__title"> Find, Book or Rent a Car Quickly and Easily!!
+//             </h1>
 
-            <p className="hero__subtitle">
-                Streamline Your Car rental experience with our Effortless Booking Process
-            </p>
+//             <p className="hero__subtitle">
+//                 Streamline Your Car rental experience with our Effortless Booking Process
+//             </p>
 
-            <CustomButton
-                title="Explore Cars"
-                containerStyles="bg-primary-blue text-white rounded-full mt-10"
-                handleClick={handleScroll}
-            />
-            <div className="hero__image-container">
-                <div className="hero__image">
-                    <Image src="/hero.png" alt="hero" fill className="object-contain" />
-                </div>
-            </div>
-        </div>
-    </div>
-  )
+//             <CustomButton
+//                 title="Explore Cars"
+//                 containerStyles="bg-primary-blue text-white rounded-full mt-10"
+//                 handleClick={handleScroll}
+//             />
+//             <div className="hero__image-container">
+//                 <div className="hero__image">
+//                     <Image src="/hero.png" alt="hero" fill className="object-contain" />
+//                     <div className="hero__image-overlay"/>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//   )
 }
 
 export default Hero
