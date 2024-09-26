83a6bd20282744d64d8077e4724915b3cd7ee6bb
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-27 02:17:31 +0300

    changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="flex flex-col text-black">
+      
+    </footer>
   )
 }
 
