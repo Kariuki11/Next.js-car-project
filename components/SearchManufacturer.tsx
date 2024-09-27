2208428902afd5e604dd7c7501986ba0d489347d
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-27 16:43:05 +0300

    "Added 'use client' directive, imported Image from next/image, and replaced dummy SearchManufacturer component with Combobox and Image elements"

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,27 @@
+"use client";
+
+import { Combobox, Transition } from '@headlessui/react'
 import { SearchManufacturerProps } from '@/types'
-import React from 'react'
+import Image from "next/image";
+
 
 const SearchManufacturer = ({ manufacturer, setManufacturer}: SearchManufacturerProps ) => {
   return (
-    <div>SearchManufacturer</div>
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image 
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car Logo"
+            />
+          </Combobox.Button>
+        </div>
+      </Combobox>
+    </div>
   )
 }
 
