84883f667b505c88838e6a449ada195a9b3c5c69
Kariuki_Ken <irungukenneth433@gmail.com>
2024-10-09 00:50:24 +0300

    Refactor car data fetching utility to use fetch API and simplify request options

--- utils/index.ts
+++ utils/index.ts
@@ -1,18 +1,20 @@
-import axios from 'axios';
-
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
 const options = {
-  method: 'GET',
-  url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
-  params: {model: 'corolla'},
-  headers: {
-    'x-rapidapi-key': 'f708da4cb0mshe1793a72de5706cp10326cjsn489060721eed',
-    'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
-  }
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'f708da4cb0mshe1793a72de5706cp10326cjsn489060721eed',
+		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
 };
 
 try {
-	const response = await axios.request(options);
-	console.log(response.data);
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
 } catch (error) {
 	console.error(error);
+}
+
+export async function fetchCars() {
+    
 }
