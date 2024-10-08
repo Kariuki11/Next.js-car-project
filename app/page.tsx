0bf3c45e983f1c8a9041cf03435598f6d94e0bfc
Kariuki_Ken <irungukenneth433@gmail.com>
2024-10-09 01:07:32 +0300

    "Updated Home function to fetch cars data and modified fetchCars function in utils/index.ts"

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import { CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty
+
   return (
     <main className="overflow-hidden">
       <Hero />
--- utils/index.ts
+++ utils/index.ts
@@ -1,27 +1,10 @@
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'f708da4cb0mshe1793a72de5706cp10326cjsn489060721eed',
-		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-
 export async function fetchCars() {
     const headers = {
             'x-rapidapi-key': 'f708da4cb0mshe1793a72de5706cp10326cjsn489060721eed',
             'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
         }
 
-    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
         headers: headers,
     });
 
