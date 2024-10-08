a3550264cd7348387d01f1c0aa5e25cf6924f0c1
Kariuki_Ken <irungukenneth433@gmail.com>
2024-10-09 00:57:51 +0300

    "Added headers and implementation to fetchCars function"

--- utils/index.ts
+++ utils/index.ts
@@ -16,5 +16,16 @@ try {
 }
 
 export async function fetchCars() {
-    
+    const headers = {
+            'x-rapidapi-key': 'f708da4cb0mshe1793a72de5706cp10326cjsn489060721eed',
+            'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+        }
+
+    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+        headers: headers,
+    });
+
+    const result = await response.json();
+
+    return result;
 }
