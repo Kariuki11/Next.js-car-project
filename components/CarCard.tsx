f249031b7efc34b5b5a73e55f0e3740715d9c267
Kariuki_Ken <irungukenneth433@gmail.com>
2024-10-09 15:47:45 +0300

    Add "View More" button to CarCard component for enhanced user interaction

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -60,6 +60,15 @@ const CarCard = ({ car }: CarCardProps) => {
                         </p>
                     </div>
                 </div>
+
+                <div className="car-card__btn-container">
+                    <CustomButton
+                        title="View More"
+                        containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+                        textStyles="text-white text-[14px] leading-[17px] font-bold"
+                        rightIcon="/right-arrow.svg"
+                    />
+                </div>
             </div>
 
         </div>
