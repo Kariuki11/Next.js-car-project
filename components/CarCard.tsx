2b4133d4cb9f4e097f1e64d6f13a1de49e930733
Kariuki_Ken <irungukenneth433@gmail.com>
2024-10-09 16:10:14 +0300

    Add CarDetails component and integrate it into CarCard; update index exports

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -6,6 +6,7 @@ import { calculateCarRent } from '@/utils';
 import { CarProps } from '@/types';
 
 import CustomButton from "./CustomButton";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -65,7 +66,7 @@ const CarCard = ({ car }: CarCardProps) => {
         </div>
       </div>
 
-      {/* <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} /> */}
+      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
     </div>
   );
 };
