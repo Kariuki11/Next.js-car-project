f50aadd4a1c5b5621ae1b09d6c972c7a0c9ca584
Kariuki_Ken <irungukenneth433@gmail.com>
2024-10-10 11:05:57 +0300

    Enhance CarDetails component by adding close button with styling in modal

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -39,11 +39,20 @@ const CarDetails = ({ isOpen, closeModal, car}: CarDetailsProps) => {
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-            <Dialog.Panel>
+            <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xsl transition-all flex flex-col gap-5">
               <button
                 type="button"
+                className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
                 onClick={closeModal}
-              ></button>
+              >
+                <Image
+                  src="/close.svg"
+                  alt="close"
+                  width={20}
+                  height={20}
+                  className="object-contain"
+                />
+              </button>
             </Dialog.Panel>
           </Transition.Child>
           </div>
