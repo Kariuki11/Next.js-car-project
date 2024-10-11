ed1c20c4c14ad665139b9da6b7e783e9c782e4b2
Kariuki_Ken <irungukenneth433@gmail.com>
2024-10-11 11:08:52 +0300

    Add SearchButton component with magnifying glass icon to SearchBar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,6 +3,19 @@
 import { useState } from 'react'
 import { SearchManufacturer } from './'
 import React from 'react'
+import Image from 'next/image'
+
+const SearchButton = ({ otherClasses }: {otherClasses: string })=> (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+)
 
 const SearchBar = () => {
   const[manufacturer, setManufacturer] = useState('');
@@ -14,9 +27,13 @@ const SearchBar = () => {
     <form className='searchbar' onSubmit=
     {handleSearch}>
         <div className="searchbar__item">
-          <SearchManufacturer/>  
+          <SearchManufacturer
+            manufacturer={manufacturer}
+            setManufacturer={setManufacturer}
+          />  
+          <SearchButton otherClasses="sm:hidden" />
         </div>
-
+        <div className="searchbar__item"></div>
     </form>
   )
 }
