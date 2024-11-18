fc0ed1898b905152f9dccf8746634f0c2c37ecd1
Kariuki 11 <irungukenneth433@gmail.com>
2024-11-18 15:34:27 +0300

    Refactor fetchCars function to utilize filtering parameters and update Home component to handle searchParams

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,14 @@
 import { CarProps, FilterProps } from "@/types";
 
 export async function fetchCars(filters: FilterProps) {
+    const { manufacturer, year, fuel, model, limit } = filters;
+
     const headers = {
             'x-rapidapi-key': 'f708da4cb0mshe1793a72de5706cp10326cjsn489060721eed',
             'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
         }
 
-    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=forester', {
+    const response = await fetch (`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=$limit=${limit}&fuel_type=${fuel}`, {
         headers: headers,
     });
 
