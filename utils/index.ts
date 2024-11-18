4930e703d6c896c11802c4ec12046d31b8454f9c
Kariuki 11 <irungukenneth433@gmail.com>
2024-11-18 15:18:54 +0300

    Refactor Home component to accept search parameters and update fetchCars function to support filtering

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,6 @@
 import { CarProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
     const headers = {
             'x-rapidapi-key': 'f708da4cb0mshe1793a72de5706cp10326cjsn489060721eed',
             'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
